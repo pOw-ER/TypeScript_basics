0b6fb36403964cb76b40adef4859d9d3a737df87
Enes Yilmaz <enesyilmaz60@hotmail.com>
2020-10-23 23:41:57 +0200

    functions

---
+++ typescript/functions.js
@@ -0,0 +1,28 @@
+// function getAverage (a:number,b:number,c?:number):string {// burda ? o secenegi optional yapar yani 3. veri girilmese hata vermez. string ise sonucu string olarak almamizi saglar
+//   let total = a+b;
+//   let count = 2;
+//   if (typeof c !== 'undefined'){
+//     total+=c;
+//     count++;
+//   }
+//   const result = total/count;
+//   return 'result : '+ result;
+// }
+// getAverage(10,20);// c ye ? koydugumuz icin hata vermedi 2 deger ile islem yapmamiza
+// getAverage(10,20,30);
+var getAverage = function () {
+    var a = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        a[_i] = arguments[_i];
+    }
+    //  geriye deger döndürmeyeceksek string yerine void yazariz
+    var total = 0;
+    var count = 0;
+    for (var i = 0; i < a.length; i++) { // degere bagimliligimiz kalmadi for döngüsüyle girilen her deger burda toplanip hesaplanir ve her seferinde count bir artar
+        total += a[i];
+        count++;
+    }
+    var result = total / count;
+    return 'result : ' + result;
+};
+getAverage(10, 20, 30, 40, 50, 60, 70); // istedigimiz kadar deger girebiliriz
---
+++ typescript/functions.ts
@@ -0,0 +1,32 @@
+// function getAverage (a:number,b:number,c?:number):string {// burda ? o secenegi optional yapar yani 3. veri girilmese hata vermez. string ise sonucu string olarak almamizi saglar
+//   let total = a+b;
+//   let count = 2;
+
+//   if (typeof c !== 'undefined'){
+//     total+=c;
+//     count++;
+//   }
+
+//   const result = total/count;
+
+//   return 'result : '+ result;
+// }
+
+// getAverage(10,20);// c ye ? koydugumuz icin hata vermedi 2 deger ile islem yapmamiza
+// getAverage(10,20,30);
+
+const getAverage = (...a:number[]):string => {//burda a number array i ile fonksiyona istedigimiz kadar deger girebilir yaptik. ve arrow function a cevirdik.
+  //  geriye deger döndürmeyeceksek string yerine void yazariz
+  let total = 0;
+  let count = 0;
+
+  for (let i = 0; i < a.length;i++){ // degere bagimliligimiz kalmadi for döngüsüyle girilen her deger burda toplanip hesaplanir ve her seferinde count bir artar
+    total += a[i];
+    count ++;
+  }
+
+  const result = total/count;
+
+  return 'result : '+ result;
+}
+getAverage(10,20,30,40,50,60,70);// istedigimiz kadar deger girebiliriz
