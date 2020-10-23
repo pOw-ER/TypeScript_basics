490a9eca3f6cdb916ae5f0bde719357d492f3a6e
Enes Yilmaz <enesyilmaz60@hotmail.com>
2020-10-23 22:28:40 +0200

    ts to js

---
+++ typescript/greeter.ts
@@ -0,0 +1,8 @@
+function greeter (name){
+  console.log('hello '+name);
+}
+
+let user = 'sadik';
+
+greeter(user); // bunu tsc(typescript compiler) ile terminalde js dosyasina otomatik cevirir. ts dosyalari tarayici tarafindan algilanamz
+// sonra da node greeter.js ile calistiririz
