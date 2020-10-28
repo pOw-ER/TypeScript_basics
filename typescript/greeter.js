8d4206e772d9392ea8b2200784dc3cb193a9b532
Enes Yilmaz <enesyilmaz60@hotmail.com>
2020-10-28 22:52:41 +0100

    function

--- typescript/greeter.js
+++ typescript/greeter.js
@@ -3,3 +3,19 @@ function greeter(name) {
 }
 var user = 'sadik';
 greeter(user);
+
+
+function grüßen(name,sex){
+  if(sex==='male'){
+    sex = 'Mr.'
+  }
+  if(sex==='female'){
+    sex='Miss'
+  }
+  console.log('hello '+sex+' '+name);
+}
+
+
+
+grüßen('Büsra','female');
+
