1aacfdc90a66b15aa7b29afa6de8a4bfe8b662cd
Enes Yilmaz <enesyilmaz60@hotmail.com>
2020-11-06 20:31:26 +0100

    functions

--- exercise/ProductService.ts
+++ exercise/ProductService.ts
@@ -1,18 +1,28 @@
 import { IProductService } from "./IProductService";
 import { Product } from "./Product";
+import { SimpleDataSource } from "./SimpleDataSource";
 
 class ProductService implements IProductService{
+  private dataSource: SimpleDataSource;
+  private products : Array<Product>;
+
+  constructor(){
+    this.dataSource = new SimpleDataSource();
+    this.products = new Array<Product>();
+    this.dataSource.getProducts().forEach(p => this.products.push(p));
+  }
+
   getById(id: number): Product {
-    throw new Error("Method not implemented.");
+    return this.products.filter(p => p.id === id)[0];
   }
   getProducts(): Product[] {
-    throw new Error("Method not implemented.");
+
   }
   saveProduct(product: Product): void {
-    throw new Error("Method not implemented.");
+
   }
   deleteProduct(product: Product): void {
-    throw new Error("Method not implemented.");
+
   }
 
 }
